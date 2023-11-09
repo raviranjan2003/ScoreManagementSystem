08bcd0ff16bfe7672121c896ae36a68c93917a6e
Ravi <2140131@sliet.ac.in>
2023-11-10 00:58:53 +0530

    connected with mongodb Database

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 const PORT = 3000;
+const uri = "mongodb+srv://ravidemo3:Ravi2003@cluster0.huhsgu8.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.connect(uri)
+.then(()=>console.log("Database Connected Successfully !"))
+.catch((err)=>console.log(err))
 
 app.use('/', require('./routes'))
 
